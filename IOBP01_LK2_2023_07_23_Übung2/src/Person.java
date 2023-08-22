a340e9cccae941c487b303dcd082064c5f672ab5
DenisJakupovic <denis.jakupovic@gmx.ch>
2023-08-22 19:28:54 +0200

    IU-IOBP01_LK2_E2

---
+++ IOBP01_LK2_2023_07_23_Übung2/src/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+	private String name;
+	private boolean vip = false;
+	private int age;
+	
+	public void setName (String n) {
+		name = n;
+		}
+	public String getName () {
+		return name;
+	}
+	public void setVip (boolean v) {
+		vip = v;
+	}
+	public void setAge (int a) {
+		age = a;
+	}
+	public boolean getVip () {
+		return vip;
+	}
+	public int getAge () {
+		return age;
+	}
+	
+	
+	
+	
+}
---
+++ IOBP01_LK2_2023_07_23_Übung2/src/TestPerson.java
@@ -0,0 +1,15 @@
+
+public class TestPerson {
+
+	public static void main(String[] args) {
+		Person p1 = new Person();
+		p1.setName("Max");
+
+		
+		
+		System.out.println("Name des Opfers "+p1.getName());
+
+
+	}
+
+}
